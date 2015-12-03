9f82550d5bb937f49826fc4f5c33842ab70f84ca
Gokhan Derala <gderala@gmail.com>
2015-12-03 18:14:29 +0100

    add avaibility model and controller and db

---
+++ app/controllers/avaibilities_controller.rb
@@ -0,0 +1,34 @@
+class AvaibilitiesController < ApplicationController
+  before_action :set_equipment
+
+  def new
+    @equipment = Equipment.new
+  end
+
+  def show
+    @avaibility = Avaibility.new
+  end
+
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_equipment
+    @equipment = Equipment.find(params[:equipment_id])
+  end
+
+  def equipment_params
+    params.require(:equipment).permit(:name, :value, :description, :picture, :address)
+  end
+end
