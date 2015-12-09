34982658e30e7d965ebfece26ac56398048906da
Kai Ting Lin <lin.kaiting@gmail.com>
2015-12-09 18:27:41 +0100

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,18 @@ ActiveRecord::Schema.define(version: 20151209142605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "availabilities", force: :cascade do |t|
+    t.integer  "equipment_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
+
+  add_index "availabilities", ["equipment_id"], name: "index_availabilities_on_equipment_id", using: :btree
+  add_index "availabilities", ["user_id"], name: "index_availabilities_on_user_id", using: :btree
+
   create_table "equipment", force: :cascade do |t|
     t.string   "name"
     t.integer  "value"
@@ -80,6 +92,7 @@ ActiveRecord::Schema.define(version: 20151209142605) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "availabilities", "equipment"
   add_foreign_key "equipment", "users"
   add_foreign_key "reservations", "equipment"
   add_foreign_key "reservations", "users"
